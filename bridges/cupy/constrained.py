5e5ab5b3dbdb94e6915a08f02091a77348030c8b
njwfish <njwfish@node1702.inband>
2025-07-17 12:24:14 -0400

    device support for multigpu

--- bridges/cupy/constrained.py
+++ bridges/cupy/constrained.py
@@ -22,58 +22,63 @@ class SkellamMeanConstrainedBridge:
         schedule_type: str = "linear",
         mh_sweeps: int = 75,
         backend: str = "torch",
+        device: int = 0,
         **schedule_kwargs,
     ):
-        self.n_steps        = n_steps
-        self.m_sampler      = m_sampler
-        self.schedule_type  = schedule_type
-        self.time_points    = cp.linspace(0, 1, num=n_steps + 1)
-        self.weights        = make_weight_schedule(
-            n_steps, schedule_type, **schedule_kwargs
-        )
-        self.mh_sweeps      = mh_sweeps
-        self.backend        = backend
+        self.device          = device
+        self.n_steps         = n_steps
+        self.mh_sweeps       = mh_sweeps
+        self.backend         = backend
+        self.m_sampler       = m_sampler
+        self.schedule_type   = schedule_type
+
+        with cp.cuda.Device(self.device):
+            self.time_points = cp.linspace(0, 1, num=n_steps + 1)
+            self.weights     = make_weight_schedule(
+                n_steps, schedule_type, **schedule_kwargs
+            )
+
 
     def __call__(self, x_0, x_1, t_target=None):
-        # unpack
-        x_0, x_1 = dlpack_backend(x_0, x_1, backend='cupy', dtype="int32")
+        with cp.cuda.Device(self.device):
+            x_0, x_1 = dlpack_backend(x_0, x_1, backend='cupy', dtype="int32")
 
-        b, d   = x_0.shape
+            b, d   = x_0.shape
 
-        # latent (N, M, B1)
-        diff = x_1 - x_0
-        M = self.m_sampler(diff)
-        N_1 = cp.abs(diff) + 2 * M
-        B_1 = (N_1 + diff) // 2
+            # latent (N, M, B1)
+            diff = x_1 - x_0
+            M = self.m_sampler(diff)
+            N_1 = cp.abs(diff) + 2 * M
+            B_1 = (N_1 + diff) // 2
 
-        # choose time index - either specified or random
-        if t_target is not None:
-            # Find closest grid point to target time
-            time_diffs = cp.abs(self.time_points - t_target)
-            k = cp.argmin(time_diffs)
-        else:
-            k = cp.random.randint(1, self.n_steps + 1)
+            # choose time index - either specified or random
+            if t_target is not None:
+                # Find closest grid point to target time
+                time_diffs = cp.abs(self.time_points - t_target)
+                k = cp.argmin(time_diffs)
+            else:
+                k = cp.random.randint(1, self.n_steps + 1)
 
-        t      = cp.broadcast_to(self.time_points[k], (b, 1))                          # (b,)
-        w_t    = self.weights[k]                              # (b,)
+            t      = cp.broadcast_to(self.time_points[k], (b, 1))                          # (b,)
+            w_t    = self.weights[k]                              # (b,)
 
-        Nt_tot = (w_t * N_1.sum(axis=0)).round().astype(cp.int32)
-        Bt_tot = (w_t * B_1.sum(axis=0)).round().astype(cp.int32)
+            Nt_tot = (w_t * N_1.sum(axis=0)).round().astype(cp.int32)
+            Bt_tot = (w_t * B_1.sum(axis=0)).round().astype(cp.int32)
 
-        N_t = mvhg(pop=N_1.T, draws_tot=Nt_tot).T
-        B_t = ffh(w=B_1.T, b=(N_1 - B_1).T, k=N_t.T, S=Bt_tot, sweeps=self.mh_sweeps).T
+            N_t = mvhg(pop=N_1.T, draws_tot=Nt_tot).T
+            B_t = ffh(w=B_1.T, b=(N_1 - B_1).T, k=N_t.T, S=Bt_tot, sweeps=self.mh_sweeps).T
 
-        x_t = x_1 - 2 * (B_1 - B_t) + (N_1 - N_t)
+            x_t = x_1 - 2 * (B_1 - B_t) + (N_1 - N_t)
 
-        # recompute diff & slack pairs after MH (keeps N_t fixed)
-        diff_t = (x_t - x_0)
-        M_t    = (N_t - cp.abs(diff_t)) // 2
+            # recompute diff & slack pairs after MH (keeps N_t fixed)
+            diff_t = (x_t - x_0)
+            M_t    = (N_t - cp.abs(diff_t)) // 2
 
-        out = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
-        # cp.get_default_memory_pool().free_all_blocks()
-        # cp.get_default_pinned_memory_pool().free_all_blocks()
-        # cp.cuda.Stream.null.synchronize()
-        return out
+            out = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
+            # cp.get_default_memory_pool().free_all_blocks()
+            # cp.get_default_pinned_memory_pool().free_all_blocks()
+            # cp.cuda.Stream.null.synchronize()
+            return out
 
     def reverse_sampler(
         self,
@@ -96,85 +101,86 @@ class SkellamMeanConstrainedBridge:
         the state is projected onto the hyper-plane
             { ‖x‖₁  =  round(B · μ_k) }.
         """
-        b, d = x_1.shape
-        x_1 = cp.from_dlpack(x_1).round().astype(cp.int32)
-        mu_0 = cp.from_dlpack(mu_0)
-        if guidance_x_0 is not None:
-            guidance_x_0 = cp.from_dlpack(guidance_x_0).round().astype(cp.int32)
-
-        mu_1 = x_1.mean(axis=0)
-        S_0 = (mu_0 * b).round().astype(cp.int32)
-
-        def sample_step(k, x_t, M_t=None, **z):
-            if M_t is None:
-                if not self.m_sampler.markov:
-                    M_t = self.m_sampler(x_t)
-
-            t = cp.broadcast_to(self.time_points[k], (b, 1))
-
-            x_t_dl, M_t_dl, t_dl = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
-            x0_hat_t = model.sample(x_t_dl, M_t_dl, t_dl, **z)
-            x0_hat_t = cp.from_dlpack(x0_hat_t)
-
-
+        with cp.cuda.Device(self.device):
+            b, d = x_1.shape
+            x_1 = cp.from_dlpack(x_1).round().astype(cp.int32)
+            mu_0 = cp.from_dlpack(mu_0)
             if guidance_x_0 is not None:
-                x0_hat_t =  guidance_schedule[k] * guidance_x_0 + (1 - guidance_schedule[k]) * x0_hat_t
+                guidance_x_0 = cp.from_dlpack(guidance_x_0).round().astype(cp.int32)
 
-            x0_hat_t = x0_hat_t.round().astype(cp.int32)
-            x0_hat_t = proportional_proj(x0_hat_t.T, S_0).T
-            diff = x_t - x0_hat_t
+            mu_1 = x_1.mean(axis=0)
+            S_0 = (mu_0 * b).round().astype(cp.int32)
 
-            # the markov sampler requires the diff to sample M_t
-            if M_t is None:
-                M_t = self.m_sampler(diff)
-            N_t         = cp.abs(diff) + 2 * M_t
-            B_t         = (N_t + diff) // 2
+            def sample_step(k, x_t, M_t=None, **z):
+                if M_t is None:
+                    if not self.m_sampler.markov:
+                        M_t = self.m_sampler(x_t)
 
-            ρ = (self.weights[k-1] / self.weights[k])
+                t = cp.broadcast_to(self.time_points[k], (b, 1))
 
-            N_s_tot = (ρ * N_t.sum(axis=0)).round().astype(cp.int32)
-            B_s_tot = (ρ * B_t.sum(axis=0)).round().astype(cp.int32)
+                x_t_dl, M_t_dl, t_dl = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
+                x0_hat_t = model.sample(x_t_dl, M_t_dl, t_dl, **z)
+                x0_hat_t = cp.from_dlpack(x0_hat_t)
 
-            # print("Pre-mvhg", "N_t", N_t.max(), N_t.min(), N_t.mean(), "N_s_tot", N_s_tot.max(), N_s_tot.min(), N_s_tot.mean())
-            N_s = mvhg(pop=N_t.T, draws_tot=N_s_tot).T
 
-            # print("Post-mvhg", "N_s", N_s.max(), N_s.min(), N_s.mean(), "tot diff", cp.abs(N_s.sum(axis=0) - N_s_tot).max())
-            # print("Pre-ffh", "B_t", B_t.max(), B_t.min(), B_t.mean(), "B_s_tot", B_s_tot.max(), B_s_tot.min(), B_s_tot.mean())
-            assert (B_s_tot <= cp.minimum(B_t.sum(0), N_s.sum(0))).all()
-            assert (B_s_tot >= cp.maximum(0,   N_s - (N_t - B_t)).sum(0)).all()
-            B_s = ffh(
-                w=B_t.T, b=(N_t - B_t).T, k=N_s.T, S=B_s_tot, 
-                sweeps=self.mh_sweeps
-            ).T
-            # print("N_s valid", (N_s >= 0).all(), (N_s <= N_t).all(), (N_s.sum(axis=0) == N_s_tot).all())
-            # print("B_s valid", (B_s >= 0).all(), (B_s <= N_s).all(), (N_t - B_t >= N_s - B_s).all(), (B_s.sum(axis=0) == B_s_tot).all())
-            # print("Post-ffh", "B_s", B_s.max(), B_s.min(), B_s.mean(), "tot diff", cp.abs(B_s.sum(axis=0) - B_s_tot).max())
+                if guidance_x_0 is not None:
+                    x0_hat_t =  guidance_schedule[k] * guidance_x_0 + (1 - guidance_schedule[k]) * x0_hat_t
 
-            x_s = x_t - 2 * (B_t - B_s) + (N_t - N_s) 
-            
+                x0_hat_t = x0_hat_t.round().astype(cp.int32)
+                x0_hat_t = proportional_proj(x0_hat_t.T, S_0).T
+                diff = x_t - x0_hat_t
 
-            diff_s = cp.abs(x_s - x0_hat_t) 
-            # due to some subtle rounding and parity issues we can sometimes get M_s == -1
-            M_s    = (N_s - diff_s) // 2
-            # print("diff_s", (N_s - cp.abs(x_s - x0_hat_t)).min(), (N_s - B_s).min())
-            assert (M_s >= 0).all()
-            assert ((B_s <= N_s).all())
+                # the markov sampler requires the diff to sample M_t
+                if M_t is None:
+                    M_t = self.m_sampler(diff)
+                N_t         = cp.abs(diff) + 2 * M_t
+                B_t         = (N_t + diff) // 2
 
-            # print("Post-ffh", "M_s", M_s.max(), M_s.min(), M_s.mean(), "N_s - diff_s", (N_s - diff_s).max(), (N_s - diff_s).min(), (N_s - diff_s).mean())
-            return x_s, M_s, x0_hat_t
+                ρ = (self.weights[k-1] / self.weights[k])
 
-        x_t, M_t, x0_hat_t = sample_step(self.n_steps, x_1, **z)
+                N_s_tot = (ρ * N_t.sum(axis=0)).round().astype(cp.int32)
+                B_s_tot = (ρ * B_t.sum(axis=0)).round().astype(cp.int32)
 
-        traj, xhat_traj, M_traj = [x_t], [x0_hat_t], [M_t]
-        for k in range(self.n_steps - 1, 0, -1):
-            x_t, M_t, x0_hat_t = sample_step(k, x_t, M_t, **z)
+                # print("Pre-mvhg", "N_t", N_t.max(), N_t.min(), N_t.mean(), "N_s_tot", N_s_tot.max(), N_s_tot.min(), N_s_tot.mean())
+                N_s = mvhg(pop=N_t.T, draws_tot=N_s_tot).T
 
-            if return_trajectory: traj.append(x_t)
-            if return_x_hat:      xhat_traj.append(x0_hat_t)
-            if return_M:          M_traj.append(M_t)
+                # print("Post-mvhg", "N_s", N_s.max(), N_s.min(), N_s.mean(), "tot diff", cp.abs(N_s.sum(axis=0) - N_s_tot).max())
+                # print("Pre-ffh", "B_t", B_t.max(), B_t.min(), B_t.mean(), "B_s_tot", B_s_tot.max(), B_s_tot.min(), B_s_tot.mean())
+                assert (B_s_tot <= cp.minimum(B_t.sum(0), N_s.sum(0))).all()
+                assert (B_s_tot >= cp.maximum(0,   N_s - (N_t - B_t)).sum(0)).all()
+                B_s = ffh(
+                    w=B_t.T, b=(N_t - B_t).T, k=N_s.T, S=B_s_tot, 
+                    sweeps=self.mh_sweeps
+                ).T
+                # print("N_s valid", (N_s >= 0).all(), (N_s <= N_t).all(), (N_s.sum(axis=0) == N_s_tot).all())
+                # print("B_s valid", (B_s >= 0).all(), (B_s <= N_s).all(), (N_t - B_t >= N_s - B_s).all(), (B_s.sum(axis=0) == B_s_tot).all())
+                # print("Post-ffh", "B_s", B_s.max(), B_s.min(), B_s.mean(), "tot diff", cp.abs(B_s.sum(axis=0) - B_s_tot).max())
 
-        outs = [x_t]
-        if return_trajectory: outs.append(cp.stack(traj))
-        if return_x_hat:      outs.append(cp.stack(xhat_traj))
-        if return_M:          outs.append(cp.stack(M_traj))
-        return tuple(outs) if len(outs) > 1 else x_t 
+                x_s = x_t - 2 * (B_t - B_s) + (N_t - N_s) 
+                
+
+                diff_s = cp.abs(x_s - x0_hat_t) 
+                # due to some subtle rounding and parity issues we can sometimes get M_s == -1
+                M_s    = (N_s - diff_s) // 2
+                # print("diff_s", (N_s - cp.abs(x_s - x0_hat_t)).min(), (N_s - B_s).min())
+                assert (M_s >= 0).all()
+                assert ((B_s <= N_s).all())
+
+                # print("Post-ffh", "M_s", M_s.max(), M_s.min(), M_s.mean(), "N_s - diff_s", (N_s - diff_s).max(), (N_s - diff_s).min(), (N_s - diff_s).mean())
+                return x_s, M_s, x0_hat_t
+
+            x_t, M_t, x0_hat_t = sample_step(self.n_steps, x_1, **z)
+
+            traj, xhat_traj, M_traj = [x_t], [x0_hat_t], [M_t]
+            for k in range(self.n_steps - 1, 0, -1):
+                x_t, M_t, x0_hat_t = sample_step(k, x_t, M_t, **z)
+
+                if return_trajectory: traj.append(x_t)
+                if return_x_hat:      xhat_traj.append(x0_hat_t)
+                if return_M:          M_traj.append(M_t)
+
+            outs = [x_t]
+            if return_trajectory: outs.append(cp.stack(traj))
+            if return_x_hat:      outs.append(cp.stack(xhat_traj))
+            if return_M:          outs.append(cp.stack(M_traj))
+            return tuple(outs) if len(outs) > 1 else x_t 
--- bridges/cupy/skellam.py
+++ bridges/cupy/skellam.py
@@ -16,61 +16,68 @@ class SkellamBridge:
         schedule_type: str = "linear",
         homogeneous_time: bool = False,
         backend: str = "torch",
+        device: int = 0,
         **schedule_kwargs,
     ):
+        self.device           = device
         self.n_steps          = n_steps
         self.m_sampler        = m_sampler
         self.schedule_type    = schedule_type
-        self.time_points      = cp.linspace(0, 1, n_steps + 1)
+        # this is really only for comparison with the constrained bridge
+        # there is no need to sample homogeneous time points in this bridge
         self.homogeneous_time = homogeneous_time
-        self.weights          = make_weight_schedule(
-            n_steps, schedule_type, **schedule_kwargs
-        )
         self.backend          = backend
+        with cp.cuda.Device(self.device):
+
+            self.time_points      = cp.linspace(0, 1, n_steps + 1)
+            self.weights          = make_weight_schedule(
+                n_steps, schedule_type, **schedule_kwargs
+            )
 
     def __call__(self, x_0, x_1, t_target=None):
-        x_0, x_1 = dlpack_backend(x_0, x_1, backend='cupy', dtype="int32")
+        with cp.cuda.Device(self.device):
+            x_0, x_1 = dlpack_backend(x_0, x_1, backend='cupy', dtype="int32")
 
-        b, d = x_0.shape
+            b, d = x_0.shape
 
-        diff = (x_1 - x_0)
-        M = self.m_sampler(diff)
-        N_1 = cp.abs(diff) + 2 * M
-        B_1 = (N_1 + diff) // 2
+            diff = (x_1 - x_0)
+            M = self.m_sampler(diff)
+            N_1 = cp.abs(diff) + 2 * M
+            B_1 = (N_1 + diff) // 2
 
-        if t_target is not None:
-            # find closest time point
-            time_diffs = cp.abs(self.time_points - t_target)
-            k = cp.argmin(time_diffs)
-            k = cp.broadcast_to(k, (b,))
+            if t_target is not None:
+                # find closest time point
+                time_diffs = cp.abs(self.time_points - t_target)
+                k = cp.argmin(time_diffs)
+                k = cp.broadcast_to(k, (b,))
 
-        elif self.homogeneous_time:
-            k = cp.random.randint(1, self.n_steps + 1)
-            k = cp.broadcast_to(k, (b,))
-        else:
-            k = cp.random.randint(1, self.n_steps + 1, (b,))
+            elif self.homogeneous_time:
+                k = cp.random.randint(1, self.n_steps + 1)
+                k = cp.broadcast_to(k, (b,))
+            else:
+                k = cp.random.randint(1, self.n_steps + 1, (b,))
+                
+            t     = self.time_points[k].reshape(-1, 1)
+            w_t   = self.weights[k]
             
-        t     = self.time_points[k].reshape(-1, 1)
-        w_t   = self.weights[k]
-        
-        N_t = cp.random.binomial(N_1, cp.expand_dims(w_t, -1), dtype=cp.int32)
-        
-        # need to gaurd against zero nsample
-        non_zero = N_t > 0
-        B_t = cp.zeros_like(B_1)
-        B_t[non_zero] = cp.random.hypergeometric(
-            ngood=B_1[non_zero],
-            nbad=N_1[non_zero] - B_1[non_zero],
-            nsample=N_t[non_zero], 
-            dtype=cp.int32
-        )
+            N_t = cp.random.binomial(N_1, cp.expand_dims(w_t, -1), dtype=cp.int32)
+            
+            # need to gaurd against zero nsample
+            non_zero = N_t > 0
+            B_t = cp.zeros_like(B_1)
+            B_t[non_zero] = cp.random.hypergeometric(
+                ngood=B_1[non_zero],
+                nbad=N_1[non_zero] - B_1[non_zero],
+                nsample=N_t[non_zero], 
+                dtype=cp.int32
+            )
 
-        x_t = x_1 - 2 * (B_1 - B_t) + (N_1 - N_t)
+            x_t = x_1 - 2 * (B_1 - B_t) + (N_1 - N_t)
 
-        diff_t = cp.abs(x_t - x_0)
-        M_t    = (N_t - diff_t) // 2
+            diff_t = cp.abs(x_t - x_0)
+            M_t    = (N_t - diff_t) // 2
 
-        return dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
+            return dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
     
     def reverse_sampler(
         self,
@@ -83,69 +90,70 @@ class SkellamBridge:
         guidance_x_0:      cp.ndarray = None,
         guidance_schedule: cp.ndarray = None,
     ):
-        b, d    = x_1.shape
-        x_t     = cp.from_dlpack(x_1).round().astype(cp.int32)
-
-        if guidance_x_0 is not None:
-            guidance_x_0 = cp.from_dlpack(guidance_x_0).round().astype(cp.int32)
-
-        def sample_step(k, x_t, M_t=None, **z):
-            if M_t is None:
-                if not self.m_sampler.markov:
-                    M_t = self.m_sampler(x_t)
-
-            print(k, self.time_points[k])
-            t = cp.broadcast_to(self.time_points[k], (b, 1))
-            x_t_dl, M_t_dl, t_dl = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
-            x0_hat_t = model.sample(x_t_dl, M_t_dl, t_dl, **z)
-            x0_hat_t = cp.from_dlpack(x0_hat_t)
+        with cp.cuda.Device(self.device):
+            b, d    = x_1.shape
+            x_t     = cp.from_dlpack(x_1).round().astype(cp.int32)
 
             if guidance_x_0 is not None:
-                x0_hat_t =  guidance_schedule[k] * guidance_x_0 + (1 - guidance_schedule[k]) * x0_hat_t
+                guidance_x_0 = cp.from_dlpack(guidance_x_0).round().astype(cp.int32)
 
-            x0_hat_t = x0_hat_t.round().astype(cp.int32)
-            diff = x_t - x0_hat_t
+            def sample_step(k, x_t, M_t=None, **z):
+                if M_t is None:
+                    if not self.m_sampler.markov:
+                        M_t = self.m_sampler(x_t)
 
-            # the markov sampler requires the diff to sample M_t
-            if M_t is None:
-                M_t = self.m_sampler(diff)
+                print(k, self.time_points[k])
+                t = cp.broadcast_to(self.time_points[k], (b, 1))
+                x_t_dl, M_t_dl, t_dl = dlpack_backend(x_t, M_t, t, backend=self.backend, dtype="float32")
+                x0_hat_t = model.sample(x_t_dl, M_t_dl, t_dl, **z)
+                x0_hat_t = cp.from_dlpack(x0_hat_t)
 
-            N_t         = cp.abs(diff) + 2 * M_t
-            B_t         = (N_t + diff) // 2
+                if guidance_x_0 is not None:
+                    x0_hat_t =  guidance_schedule[k] * guidance_x_0 + (1 - guidance_schedule[k]) * x0_hat_t
 
-            ρ = (self.weights[k-1] / self.weights[k])
-            non_zero = N_t > 0
-            N_s = cp.zeros_like(N_t)
-            N_s[non_zero] = cp.random.binomial(N_t[non_zero], ρ)
+                x0_hat_t = x0_hat_t.round().astype(cp.int32)
+                diff = x_t - x0_hat_t
 
-            non_zero = N_s > 0
-            B_s = cp.zeros_like(B_t)
-            B_s[non_zero] = cp.random.hypergeometric(
-                ngood   = B_t[non_zero],
-                nbad    = N_t[non_zero] - B_t[non_zero],
-                nsample = N_s[non_zero]
-            )
+                # the markov sampler requires the diff to sample M_t
+                if M_t is None:
+                    M_t = self.m_sampler(diff)
 
-            x_s = x_t - 2 * (B_t - B_s) + (N_t - N_s)
-            diff_s = cp.abs(x_s - x0_hat_t)
-            M_s = (N_s - diff_s) // 2  
+                N_t         = cp.abs(diff) + 2 * M_t
+                B_t         = (N_t + diff) // 2
 
-            return x_s, M_s, x0_hat_t
-        
-        M_t = self.m_sampler(x_1)
-        x_t, M_t, x0_hat_t = sample_step(self.n_steps, x_t, M_t, **z)
+                ρ = (self.weights[k-1] / self.weights[k])
+                non_zero = N_t > 0
+                N_s = cp.zeros_like(N_t)
+                N_s[non_zero] = cp.random.binomial(N_t[non_zero], ρ)
 
-        traj, xhat_traj, M_traj = [x_t], [x0_hat_t], [M_t]
-        for k in range(self.n_steps - 1, 0, -1):
+                non_zero = N_s > 0
+                B_s = cp.zeros_like(B_t)
+                B_s[non_zero] = cp.random.hypergeometric(
+                    ngood   = B_t[non_zero],
+                    nbad    = N_t[non_zero] - B_t[non_zero],
+                    nsample = N_s[non_zero]
+                )
+
+                x_s = x_t - 2 * (B_t - B_s) + (N_t - N_s)
+                diff_s = cp.abs(x_s - x0_hat_t)
+                M_s = (N_s - diff_s) // 2  
+
+                return x_s, M_s, x0_hat_t
             
-            x_t, M_t, x0_hat_t = sample_step(k, x_t, M_t, **z)
+            M_t = self.m_sampler(x_1)
+            x_t, M_t, x0_hat_t = sample_step(self.n_steps, x_t, M_t, **z)
 
-            if return_trajectory: traj.append(x_t)
-            if return_x_hat:     xhat_traj.append(x0_hat_t)
-            if return_M:         M_traj.append(M_t)
+            traj, xhat_traj, M_traj = [x_t], [x0_hat_t], [M_t]
+            for k in range(self.n_steps - 1, 0, -1):
+                
+                x_t, M_t, x0_hat_t = sample_step(k, x_t, M_t, **z)
 
-        outs = [x_t]
-        if return_trajectory: outs.append(cp.stack(traj))
-        if return_x_hat:      outs.append(cp.stack(xhat_traj))
-        if return_M:          outs.append(cp.stack(M_traj))
-        return tuple(outs) if len(outs) > 1 else x_t 
+                if return_trajectory: traj.append(x_t)
+                if return_x_hat:     xhat_traj.append(x0_hat_t)
+                if return_M:         M_traj.append(M_t)
+
+            outs = [x_t]
+            if return_trajectory: outs.append(cp.stack(traj))
+            if return_x_hat:      outs.append(cp.stack(xhat_traj))
+            if return_M:          outs.append(cp.stack(M_traj))
+            return tuple(outs) if len(outs) > 1 else x_t 
